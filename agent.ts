f72dc96d71bdb5d51c33b731611db14a06f38591
Abhay-0103 <ab0321054@gmail.com>
2026-03-02 01:03:58 +0530

    Refactor command execution logic and enhance message handling in run function

--- agent.ts
+++ agent.ts
@@ -14,6 +14,10 @@ function executeCommand(cmd = "") {
     return result.toString();
 }
 
+const functionMapping = {
+    executeCommand,
+}
+
 const SYSTEM_PROMPT = `You are a expert AI Assistant that is expert in controlling the user's machine. Analyise the user's query carefully and plan the steps on what needs to be done. Based on the user's query, you can create commands and then call the tool to run that command and execute on the user's machine. 
 
 Available tools:
@@ -29,27 +33,59 @@ const outputschema = z.object({
         tool_name: z.string().describe("Name of the tool"),
         params: z.array(z.string()),
     }).optional().nullable().describe("The params to call the tool if type is tool_call")
-})
+});
+
+const messages = [{
+    "role": "system",
+    "content": SYSTEM_PROMPT
+}]
 
 export async function run(query = "") {
-    const result = await openai.chat.completions.parse({
-        model: "gemini-2.5-flash",
-        text: {
-            format: zodTextFormat(outputschema, 'output'),
-        },
-        messages: [
-            {
-                "role": "system",
-                "content": SYSTEM_PROMPT
+    messages.push({ role: 'user', content: query });
+    while (true) {
+        const result = await openai.chat.completions.parse({
+            model: "gemini-2.5-flash",
+            text: {
+                format: zodTextFormat(outputschema, 'output'),
             },
-            {
-                "role": "user",
-                "content": query
+            input: messages
+        });
+        const parsedOutput = result.output_parsed
+        switch (parsedOutput.type) {
+            case 'tool_call': {
+                if (parsedOutput.tool_call) {
+                    const { params, tool_name } = parsedOutput.tool_call;
+                    console.log(`Tool Call: ${tool_name}: ${params}`);
+                    if (functionMapping[tool_name]) {
+                        const toolOutput = functionMapping[tool_name](...params);
+                        console.log(`Tool Output (${tool_name})`, toolOutput);
+                        messages.push({
+                            role: 'developer',
+                            content: JSON.stringify({
+                                tool_name,
+                                params,
+                                tool_Output : toolOutput
+                            })
+                        })
+                    }
+                }
             }
-        ]
-    });
-    console.log(`Agent Says:`, JSON.stringify(result.output_parsed, null, 2));
+                break;
+            case 'text': {
+
+            }
+                break;
+            case 'text': {
+
+            }
+                break;
+        }
+
+        console.log(`Agent Says:`, JSON.stringify(result.output_parsed, null, 2));
+    }
 }
+
+
 
 // run('Make a new folder named test');
 console.log((executeCommand('ls')));
